e7005607fe2632f54aafdc65e520e807d04a161d
Arman-Kabir <armankabir8@gmail.com>
2024-01-24 19:40:23 +0600

    index.ts cleaned products added

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,27 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
+ type Query{
+  
+ }
 `;
 
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
+
 
 const resolvers = {
-    Query: {
-        books: () => books,
-    },
+  Query: {
+
+  },
 };
 
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
