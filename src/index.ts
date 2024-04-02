e91bf91caa83d5c932cc67090412a71a56027cb3
Arman-Kabir <armankabir8@gmail.com>
2024-04-02 18:10:50 +0600

    schema explored

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { db } from './db.js';
 
 const typeDefs = `#graphql
-  
+
+  type Product{
+    id:ID!
+    name:String
+    image:String
+    description:String
+    price:Float
+    quantity:Int
+    onStock:Boolean
+    category:String
+  }
+
+  type Query{
+    products:[Product]
+  }
 `;
 const resolvers = {
   Query: {
+    products:()=>db.products
   },
 };
 
